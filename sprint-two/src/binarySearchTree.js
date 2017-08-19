9eea53073139baa4777285494f0e13ec4d9aa214
JadeVasun <jadevasun@gmail.com>
2017-08-19 11:44:01 -0700

    completed binary search up to contains.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,37 @@
 var BinarySearchTree = function(value) {
+	var searchTree = Object.create(searchTreeMethods);
+		searchTree.left = null;
+		searchTree.right = null;
+		searchTree.value = value;
+
+		return searchTree;
 };
+
+var searchTreeMethods = {};
+
+searchTreeMethods.insert = function (value) {
+	
+	if (value < this.value && this.left !== null) {
+		this.left.insert(value);
+	} else if (value < this.value && this.left === null) {
+		this.left = BinarySearchTree(value);
+	} else if (value > this.value && this.right !== null) {
+		this.right.insert(value);
+	} else if (value > this.value && this.right === null) {
+		this.right = BinarySearchTree(value);
+	}
+}
+
+searchTreeMethods.contains = function (value) {
+	if (value === this.value) {
+		return true;
+	} else if (value < this.value && this.left !== null) {
+		return this.left.contains(value);
+	} else if (value > this.value && this. right !== null) {
+		return this.right.contains(value);
+	}	
+	return false; 	
+}
 
 
 /*
