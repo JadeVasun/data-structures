4b4075ffa80c254bab6c3f9ccefc560d4bd37892
JadeVasun <jadevasun@gmail.com>
2017-08-19 09:16:09 -0700

    completed up to retrieve in hash tables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,39 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._newArray = [];
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, v);
+  //key value pair into a bucket
+  // var bucket = this._storage.get(index);
+  // if (!bucket) {
+  // 	bucket = [];
+  // 	bucket.push([k,v]);
+  // 	this._storage.set(index, bucket);
+  // } 
+
+  // if (bucket) {
+  // 	for (var i = 0; i < bucket.length; i++) {
+  // 		if (bucket[i][0] === k) {
+  // 			bucket[i][1] === v;
+  // 			return;
+  // 		}
+  // 	}
+  // }
+  // bucket.push([k,v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-};
+   return this._storage.get(index);
+  }
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
